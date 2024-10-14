029573937ff225cd8f6addbd5eed515da29ad2f9
letgotanishka <tanishkasingh835@gmail.com>
2024-10-14 10:33:15 +0530

    java script

---
+++ day5/script.js
@@ -0,0 +1,66 @@
+//function display(){
+   // alert("Hello JS");
+//}
+//var x=12;
+
+//function display(){
+    //console.log("Hello JS");
+//}
+//let x=14;
+// console.log("Hello JS",x);
+
+//display();
+// let x=12.23;
+// let y="hello";
+// let z=true;
+// let obj={
+//     'name' : 'Rahul',
+//     id: 12,
+// }
+// let ar=[12,2,3,4,4];
+// let a;
+// let b=null
+// console.log(typeof b);
+
+
+// function validate()
+// {
+//     let un=document.getElementById('un').value;
+//     let pass=document.getElementById('pass').value;
+//     if(un='admin' && pass=='admin'){
+//         document.getElementById("res").innerHTML = "Login Succeess";
+//     }
+//     else
+//      document.getElementById("res").innerHTML = "Login Failed"; 
+//}
+
+
+// let x=Number("12");
+// let str=Number("123");
+
+// console.log('12'%'3');
+
+// let a=12;
+// let b='12';
+// console.log(a==b);
+// console.log(a===b);
+
+
+// let a = () => {
+//     console.log("Hello");
+// };
+// a();
+
+// let a = (x,y) => {
+//     return (x+Y);
+// };
+
+// let x=a(12,34);
+// console.log(x);
+// let say=()=> console.log("hello");
+// say();
+function display(){
+    let p1 = document.querySelector("p");
+    p1.innerHTML = "Result";
+    p1.style.backgroundColor = "red";
+}
